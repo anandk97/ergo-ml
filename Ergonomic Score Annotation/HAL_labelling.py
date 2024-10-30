d5bc4220f72fe4e604a823850f39c86e7c813588
Anand Krishnan <107951296+anandk97uw@users.noreply.github.com>
2024-10-30 13:01:46 -0700

    Changed data import structure for HAL labelling

--- Ergonomic Score Annotation/HAL_labelling.py
+++ Ergonomic Score Annotation/HAL_labelling.py
@@ -8,77 +8,31 @@ overall_threshold = 44.8 # 10 lbs
 finger_threshold = 15 # 3.3 lbs
 window_length = 10 # In seconds
 D = 75 # Duty Cycle
-#%% Import Data from CSV
-filename = r"C:\Users\anand\Desktop\HAL TLV\ID_05\Stringer\Trial_1"
-df = pd.read_csv(r"C:\Users\anand\Desktop\HAL TLV\ID_05\Stringer\Trial_1.csv",skiprows=4)
-# df = pd.read_csv('/Users/anandkrishnan/Documents/HAL TLV Data/Non technician data/Utsav_03_29_23.csv',skiprows=4)
+#%% Import Data for a specific participant, tool, and trial
+data_fps = 60 # The data was interpolated to 60 fps
+participant_id = 1
+tool_id = 1
+trial_id = 1
+left_hand_force = np.load(r"C:\Users\anand\Desktop\Hand-intensive Manufacturing Processes Dataset\p"+str(participant_id)+" tool"+str(tool_id)+" trial"+str(trial_id)+"\processed force_detail_l.npy")
+right_hand_force = np.load(r"C:\Users\anand\Desktop\Hand-intensive Manufacturing Processes Dataset\p"+str(participant_id)+" tool"+str(tool_id)+" trial"+str(trial_id)+"\processed force_detail_r.npy")
+# The 19 hand regions are listed in order as follows: 
+# palm123, thumb12, thumb_tip, index123, index_tip, middle123, ring123, little123
+time = np.arange(0, left_hand_force.shape[0]/data_fps-1/data_fps, 1/data_fps)
+# Calculate total force for each region of the left hand, convert to N, and round to 2 decimal places
+l_palm_total_force = (np.round(np.sum(left_hand_force[:,0:3], axis=1)*4.44822, 2))
+l_thumb_total_force = (np.round(np.sum(left_hand_force[:,3:6], axis=1)*4.44822, 2))
+l_index_total_force = (np.round(np.sum(left_hand_force[:,6:10], axis=1)*4.44822, 2))
+l_middle_total_force = (np.round(np.sum(left_hand_force[:,10:13], axis=1)*4.44822, 2))
+l_ring_total_force = (np.round(np.sum(left_hand_force[:,13:16], axis=1)*4.44822, 2))
+l_little_total_force = (np.round(np.sum(left_hand_force[:,16:19], axis=1)*4.44822, 2))
 
-time = df['Time [s]']
-time = time.to_numpy(dtype = 'float')
-time = time - time[0] # Start time from 0
-# Round time to three decimal places
-time = np.round(time,2)
-# Left Hand Data
-l_palm_total_force = df['L Palm Total Force [lbs.]']
-l_palm_total_force = l_palm_total_force.to_numpy(dtype = 'float')
-l_thumb_total_force = df['L Thumb Total Force [lbs.]']
-l_thumb_total_force = l_thumb_total_force.to_numpy(dtype = 'float')
-l_index_total_force = df['L Index Total Force [lbs.]']
-l_index_total_force = l_index_total_force.to_numpy(dtype = 'float')
-l_middle_total_force = df['L Middle Total Force [lbs.]']
-l_middle_total_force = l_middle_total_force.to_numpy(dtype = 'float')
-l_ring_total_force = df['L Ring Total Force [lbs.]']
-l_ring_total_force = l_ring_total_force.to_numpy(dtype = 'float')
-l_little_total_force = df['L Pinky Total Force [lbs.]']
-l_little_total_force = l_little_total_force.to_numpy(dtype = 'float')
-# Convert to N
-l_palm_total_force = l_palm_total_force*4.44822
-l_thumb_total_force = l_thumb_total_force*4.44822
-l_index_total_force = l_index_total_force*4.44822
-l_middle_total_force = l_middle_total_force*4.44822
-l_ring_total_force = l_ring_total_force*4.44822
-l_little_total_force = l_little_total_force*4.44822
-# Round to two decimal places
-l_palm_total_force = np.round(l_palm_total_force,2)
-l_thumb_total_force = np.round(l_thumb_total_force,2)
-l_index_total_force = np.round(l_index_total_force,2)
-l_middle_total_force = np.round(l_middle_total_force,2)
-l_ring_total_force = np.round(l_ring_total_force,2)
-l_little_total_force = np.round(l_little_total_force,2)
-
-# Store all left hand data in a list with two decimal places
-
-l_hand_data = [l_palm_total_force,l_thumb_total_force,l_index_total_force,l_middle_total_force,l_ring_total_force,l_little_total_force]
-l_hand_total_force = l_palm_total_force + l_thumb_total_force + l_index_total_force + l_middle_total_force + l_ring_total_force + l_little_total_force
-
-# Right Hand Data
-r_palm_total_force = df['R Palm Total Force [lbs.]']
-r_palm_total_force = r_palm_total_force.to_numpy(dtype = 'float')
-r_thumb_total_force = df['R Thumb Total Force [lbs.]']
-r_thumb_total_force = r_thumb_total_force.to_numpy(dtype = 'float')
-r_index_total_force = df['R Index Total Force [lbs.]']
-r_index_total_force = r_index_total_force.to_numpy(dtype = 'float')
-r_middle_total_force = df['R Middle Total Force [lbs.]']
-r_middle_total_force = r_middle_total_force.to_numpy(dtype = 'float')
-r_ring_total_force = df['R Ring Total Force [lbs.]']
-r_ring_total_force = r_ring_total_force.to_numpy(dtype = 'float')
-r_little_total_force = df['R Pinky Total Force [lbs.]']
-r_little_total_force = r_little_total_force.to_numpy(dtype = 'float')
-r_palm_total_force = r_palm_total_force*4.44822
-r_thumb_total_force = r_thumb_total_force*4.44822
-r_index_total_force = r_index_total_force*4.44822
-r_middle_total_force = r_middle_total_force*4.44822
-r_ring_total_force = r_ring_total_force*4.44822
-r_little_total_force = r_little_total_force*4.44822
-# Round to two decimal places
-r_palm_total_force = np.round(r_palm_total_force,2)
-r_thumb_total_force = np.round(r_thumb_total_force,2)
-r_index_total_force = np.round(r_index_total_force,2)
-r_middle_total_force = np.round(r_middle_total_force,2)
-r_ring_total_force = np.round(r_ring_total_force,2)
-r_little_total_force = np.round(r_little_total_force,2)
-r_hand_data = [r_palm_total_force,r_thumb_total_force,r_index_total_force,r_middle_total_force,r_ring_total_force,r_little_total_force]
-r_hand_total_force = r_palm_total_force + r_thumb_total_force + r_index_total_force + r_middle_total_force + r_ring_total_force + r_little_total_force
+# Calculate total force for each region of the right hand, convert to N, and round to 2 decimal places
+r_palm_total_force = (np.round(np.sum(right_hand_force[:,0:3], axis=1)*4.44822, 2))
+r_thumb_total_force = (np.round(np.sum(right_hand_force[:,3:6], axis=1)*4.44822, 2))
+r_index_total_force = (np.round(np.sum(right_hand_force[:,6:10], axis=1)*4.44822, 2))
+r_middle_total_force = (np.round(np.sum(right_hand_force[:,10:13], axis=1)*4.44822, 2))
+r_ring_total_force = (np.round(np.sum(right_hand_force[:,13:16], axis=1)*4.44822, 2))
+r_little_total_force = (np.round(np.sum(right_hand_force[:,16:19], axis=1)*4.44822, 2))
 
 
 #%% Define Functions
@@ -187,4 +141,4 @@ df.interpolate(method='linear',inplace=True)
 # Round data to 2 decimal places
 df = df.round(3)
 # df.to_csv(filename + '_w_HAL_25Hz.csv',index=False)
-df.to_csv('test.csv',index=False)
+df.to_csv('C:\Users\anand\Desktop\test.csv',index=False)
